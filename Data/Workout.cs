db413f524fd7b1875635703f157863d5aa4e8897
Romo4ka68 <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>
2026-04-16 20:37:50 +0500

    Initial commit

---
+++ Data/Workout.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessApp.Data;
+
+public class Workout
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Desc { get; set; } = "";
+    public int Time { get; set; } // в минутах
+}
